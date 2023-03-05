8a6740a0dcbb575e6d36032613d987223b933284
Danil Shamsiev <veismahs@outlook.com>
2023-03-05 21:55:24 +0400

    feat: web-devicons

--- lua/plug/init.lua
+++ lua/plug/init.lua
@@ -32,6 +32,7 @@ return require("packer").startup(function()
     use "hrsh7th/cmp-path"
     use "hrsh7th/cmp-buffer"
     use "hrsh7th/vim-vsnip"
+    use "nvim-tree/nvim-web-devicons"
     use {
       "nvim-neo-tree/neo-tree.nvim",
       branch = "v2.x",
