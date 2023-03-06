edef31f0fed61e80b015705ca6c18bf4e7052851
Danil Shamsiev <veismahs@outlook.com>
2023-03-06 14:11:57 +0400

    feat: lua_ls

--- lua/opts/lsp.lua
+++ lua/opts/lsp.lua
@@ -76,7 +76,6 @@ local lsp_flags = {
 require("lspconfig")["rust_analyzer"].setup{
     on_attach = on_attach,
     flags = lsp_flags,
-    -- Server-specific settings...
     settings = {
       ["rust-analyzer"] = {}
     }
@@ -95,4 +94,29 @@ require("lspconfig")["tsserver"].setup{
 require("lspconfig")["eslint"].setup{
     on_attach = on_attach,
     flags = lsp_flags,
+}
+
+require("lspconfig")["lua_ls"].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
