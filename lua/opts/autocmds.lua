e50dc76b3045483569682fd03c3d57f4081e3537
Danil Shamsiev <veismahs@outlook.com>
2023-03-06 14:00:59 +0400

    feat: autostart neotree

--- lua/opts/autocmds.lua
+++ lua/opts/autocmds.lua
@@ -1,6 +1,11 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd('BufEnter', {
-  pattern = '',
-  command = 'ColorizerAttachToBuffer'
+autocmd("BufEnter", {
+  pattern = "",
+  command = "ColorizerAttachToBuffer"
+})
+
+autocmd("VimEnter", {
+  pattern = "",
+  command = "Neotree show"
 })
